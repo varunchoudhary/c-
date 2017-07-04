729765898d47f3281c1e38c25fbd8b14f78c0dbc
varunchoudhary <varun.cool46@gmail.com>
2017-07-04 20:58:18 +0530

    coding stacks class using array

--- stacks & queue/stackclassarray.h
+++ stacks & queue/stackclassarray.h
@@ -3,11 +3,24 @@ class stack{
 	T *data;
 	int capacity;
 	int size;
+	int lastindex;
+	void resize(){
+		T*temp=data;
+		data=new T[2*capacity];
+		int k=0;
+		for(int i=0;i<lastindex;i++){
+			data[k++]=temp[i];
+		}
+		lastindex=capacity-1;
+		capacity*=2;
+		delete [] temp;
+	}
 public:
 	stack(){
 		capacity=10;
 		data=new [capacity];
 		size=0;
+		lastindex=-1;
 	}
 	int count(){
 		return size;
@@ -17,10 +30,28 @@ public:
 	}
 	void push(int data){
 		if(size==capacity){
+			resize();
+		}
+		lastindex+=1;
+		data[lastindex]=data;
+		size++;
+	}
+	void pop(){
+		if(size==0){
 		//TODO;
 		}
-		data[size++]=data;
+		T output=data[lastindex];
+		lastindex--;
+		size--;
+		if(size==0){
+			lastindex=-1;
+		}
 		
-	
+	}
+	T top(){
+		if(size==0){
+		//TODO;
+		}
+		return data[lastindex];
 	}
 };
