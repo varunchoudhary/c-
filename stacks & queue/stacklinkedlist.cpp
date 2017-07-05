1911d60ec48d29a21cd1404750768fd0d9eb7b44
varunchoudhary <varun.cool46@gmail.com>
2017-07-05 10:59:42 +0530

    stackclass using linked list

---
+++ stacks & queue/stacklinkedlist.cpp
@@ -0,0 +1,44 @@
+Template<typename T>
+struct node{
+	int data;
+	node* next;
+};
+class stack{
+		node*topi;
+		int size;
+	public:
+	stack(){
+		top=NULL;
+	}
+	void push(int data);
+	void pop();
+	int top();
+	int count();
+};
+void stack::push(int data){
+	node*ptr;
+	ptr= new node;
+	ptr->data=data;
+	ptr->next=NULL;
+	if(topi!=NULL)
+		ptr->next=topi;
+	topi=ptr;
+	size++;
+}	
+void stack::pop(){
+	node*temp;
+	if(topi==NULL){
+	//TODO
+	}
+	temp=topi;
+	topi=topi->next;
+	delete temp;
+	size--;
+}
+int count(){
+	return size;
+}
+int stack::top(){
+	return topi->data;
+}
+
